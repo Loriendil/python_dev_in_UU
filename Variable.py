fcf21983832572165f9890a0b5201ba71dae1f4e
Kirill Krylov <38138392+Loriendil@users.noreply.github.com>
2024-10-10 18:21:28 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,5 @@
+amount_of_homeworks = 12
+amount_of_hours =1.5
+course = 'Python'
+
+print('Курс:', course, ', всего задач:',amount_of_homeworks,', затрачено часов:',amount_of_hours,', среднее время выполнения', amount_of_hours / amount_of_homeworks, ' часа.')
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name="Kirill"
+print("Name: ", name)
+age=38
+print("Age: ", age)
+age+=2
+print("New age: ", age)
+is_student = True
+print("Is Student: ", is_student)
