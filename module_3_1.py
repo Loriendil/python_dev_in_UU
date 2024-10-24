a97b559fa8ff362b94a3d654d51005d6fefa4920
Kirill Krylov <38138392+Loriendil@users.noreply.github.com>
2024-10-24 23:15:11 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,42 @@
+# Домашняя работа по уроку "Пространство имён"
+# Описываем сначала функции, потом основной код. Наоборот не работает, почему-то.
+# На самом деле поскольку тут нет декларирования сигнатуры метода,
+# поэтому тут собственно и не должно так работать...
+calls = 0
+
+# Опишем функцию подсчёта вызовов функций
+def count_calls():
+    global calls
+    calls += 1
+
+# Опишем функцию, считающую количество символов, выводящую слово заглавными и
+# прописными буквами
+def string_info(str1):
+    count_calls()
+    return len(str1), str1.upper(), str1.lower()
+
+# Опишем функцию, которая проверяет содержание в представленном списке слова
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    for item in list_to_search:
+        if string.lower() == item.lower():
+            return True
+    return False
+
+# Тело основной "функции"
+print('------------------------------------------------------------------------------------------------')
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
+
+print('------------------------------------------------------------------------------------------------')
+
+print(string_info('Tetrakishexahedron'))
+print(string_info('Ichthyophagous'))
+print(is_contains('Rhabdophobia', ['phob', 'phobiaIA', 'rhabdopHOBIA']))
+print(is_contains('auto', ['automotive', 'autistic']))
+print(calls)
+print('------------------------------------------------------------------------------------------------')
