6e25f6534c4437fa7cfd2b21649d00ef6fce3718
Kirill Krylov <38138392+Loriendil@users.noreply.github.com>
2025-01-28 15:22:48 +0300

    Update tests_12_4.py

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,6 +1,6 @@
 # Код для домашнего задания по теме "Логирование"
 import logging
-
+from contextlib import contextmanager
 from rt_with_exceptions import Runner
 import unittest
 
@@ -11,38 +11,46 @@ logging.basicConfig(level=logging.INFO,
                         encoding="utf-8")
 logging.info("Начало тестирования")
 
+# В уроке такая глупость рассказана. Зачем нам писать по 100 раз одно и тоже,
+# если можно показать студентам, что можно написать (ну, или напомнить / подсказать) один раз
+# обернув функцию в декораторе контекст менеджера!
+# У нас, конечно, давали ОЧЕНЬ плохо эту историю, так что впечатление об этом осталось как о заумной фигне и
+# действия лектора к такой мысли и способствуют, и поощряют, но ведь это не так!
+
+@contextmanager
+def log_exceptions(*exceptions):
+    try:
+        yield
+    except Exception as e:
+        if isinstance(e,exceptions):
+            logging.warning(f'{type(e).__name__} \n '
+                            f'Возникло исключение: {e}. Пояснение: Неверный тип данных для объекта Runner',
+                            exc_info=True)
+        else:
+            logging.critical(f'{type(e).__name__} \n Возникло исключение: {e}. '
+                             f'Пояснение: Неверный тип данных для объекта Runner', exc_info=True)
+            raise  # Re-raise if it's not one of the specified exceptions
+
 class RunnerTest(unittest.TestCase):
     is_frozen = False
 
     @unittest.skipIf(is_frozen, "Tests in this case are frozen")
     def test_walk(self):
-        try:
+        with log_exceptions(BaseException):
             first_runner = Runner("Alex", -5)
             for i in range(10):
                 first_runner.walk()
             self.assertEqual(first_runner.distance, 50)
             logging.info("test_walk выполнен успешно")
-        except TypeError as err1:
-            logging.warning(f'Неверный тип данных для объекта Runner: {err1.args}', exc_info=True)
-        except ValueError as err2:
-            logging.warning(f'Неверный тип данных для объекта Runner: {err2.args}', exc_info=True)
-        except Exception as err3:
-            logging.critical(f'Неверный тип данных для объекта Runner: {err3.args}', exc_info=True)
 
     @unittest.skipIf(is_frozen, "Tests in this case are frozen")
     def test_run(self):
-        try:
+        with log_exceptions(BaseException):
             second_runner = Runner(['Vlad'])
             for i in range(10):
                 second_runner.run()
             self.assertEqual(second_runner.distance, 100)
             logging.info('test_run" выполнен успешно')
-        except TypeError  as err4:
-            logging.warning(f'Неверный тип данных для объекта Runner: {err4.args}',  exc_info=True)
-        except ValueError  as err5:
-            logging.warning(f'Неверный тип данных для объекта Runner: {err5.args}',  exc_info=True)
-        except Exception  as err6:
-            logging.critical(f'Неверный тип данных для объекта Runner: {err6.args}', exc_info=True)
 
 
     @unittest.skipIf(is_frozen, "Tests in this case are frozen")
